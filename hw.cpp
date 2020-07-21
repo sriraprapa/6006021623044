7584a7169b4a005a00f5515194c39aad6f40861e
sriraprapa <68288235+sriraprapa@users.noreply.github.com>
2020-07-21 18:15:02 +0700

    Add files via upload

---
+++ hw.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout << "1 st Term of 2020\t" << "6006021623044\t" << "Siraprapa Lertsakwimarn\t\n";
+	cout << endl;
+	cout << "-----------------------------------------------------------------\n";
+	cout << "| Subject ID |" << "Subject name\t\t|" << "Score\t(100)\t|" << " Grade |\n" ;
+	cout << "-----------------------------------------------------------------\n";
+	cout << "|  4570034   | Introduction to computer\t|\t85\t| " << "\" A \"" << " |\n";
+	cout << "|  4580247   | Endlish\t\t\t|\t74\t| " << "\" B \"" << " |\n";
+	cout << "|  4530001   | Design Thinking\t\t|\t92\t| " << "\" A \"" << " |\n";
+	cout << "-----------------------------------------------------------------\n";
+	cout << "|   TOTAL    | Subject = 3\t\t|  Average = 3.87	|\n";
+	cout << "=====================================*===\n";
+}
